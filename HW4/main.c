5cba9259c23a1949445df45f7db92aad2818bd99
Brigitte <brigitte.broszus@gmail.com>
2018-04-18 00:07:53 -0500

    Created HW4 documents

---
+++ HW4/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: brigittebroszus
+ *
+ * Created on April 17, 2018, 11:57 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
