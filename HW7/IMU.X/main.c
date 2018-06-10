f142a9ae6d9fbf10054a993289f2d637fc590b48
Brigitte <brigitte.broszus@gmail.com>
2018-06-09 22:05:46 -0500

    HW6 and HW7

---
+++ HW7/IMU.X/main.c
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.c
+ * Author: brigittebroszus
+ *
+ * Created on June 9, 2018, 7:56 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ * When you get the correct response from WHOAMI, 
+ * write an I2CmultipleRead() function to be able 
+ * to read many registers in a row. 
+ * Read all of the temperature, gyroscope, and 
+ * accelerometer data registers, 
+ * and shift them into signed shorts 
+ * (temperature, gyroX, gyroY, gyroZ, accelX, accelY, accelZ).
+
+Read the LSM6DS33 at 20 Hz, and blink your LED as a heartbeat. Draw bars starting from the center of the LCD in the direction of the chip +X and +Y directions. Scale the length of the bars to match the readings from the accelerometer. It may help to write the text values of the readings as you debug.
+ */
+int main(int argc, char** argv) {
+
+    
+    
+    return (EXIT_SUCCESS);
+}
+
